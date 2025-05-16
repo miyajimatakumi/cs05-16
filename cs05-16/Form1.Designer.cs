87d1c9f67b4bbbacb4612fc6b9039027d48c53b2
miyajimatakumi <m.miyajima.takumi@gmail.com>
2025-05-16 15:53:19 +0900

    v2

--- cs05-16/Form1.Designer.cs
+++ cs05-16/Form1.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button4 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -45,17 +51,65 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(363, 199);
+            label1.Location = new Point(137, 38);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 1;
             label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 84);
+            button2.Name = "button2";
+            button2.Size = new Size(119, 66);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 156);
+            button3.Name = "button3";
+            button3.Size = new Size(119, 66);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 179);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(112, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 228);
+            button4.Name = "button4";
+            button4.Size = new Size(119, 66);
+            button4.TabIndex = 5;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
@@ -68,5 +122,10 @@
 
         private Button button1;
         private Label label1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button4;
     }
 }
