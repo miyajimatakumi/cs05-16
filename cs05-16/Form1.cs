c911a627809c629d7d6ecdcf7b457789b8dcdf20
miyajimatakumi <m.miyajima.takumi@gmail.com>
2025-05-16 14:31:50 +0900

    v1

--- cs05-16/Form1.cs
+++ cs05-16/Form1.cs
@@ -6,5 +6,19 @@ namespace cs05_16
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("‚±‚ñ‚É‚¿‚í");
+            MessageBox.Show(100.ToString());
+            MessageBox.Show($"{100}");
+            MessageBox.Show($"label1.Top={label1.Top}");
+            MessageBox.Show(3.14f.ToString());
+            MessageBox.Show($"{3.14f}");
+
+            MessageBox.Show(1+1.ToString());//11
+            MessageBox.Show((1+1).ToString());//2
+            MessageBox.Show($"{1 + 1}");
+        }
     }
 }
